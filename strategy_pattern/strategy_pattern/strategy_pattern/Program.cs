50c7f44a836b0347648feb317155d1461fbfd6b0
LAPTOP-DG686SC9\dany <dany19991@yahoo.com>
2018-10-07 21:22:11 +0300

    stratedy_pattern working draft

---
+++ strategy_pattern/strategy_pattern/strategy_pattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strategy_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rubberduck rubberduck = new Rubberduck();
+            rubberduck.performFly();
+            
+        }
+    }
+
+    class Duck
+    {
+        public FlyBehaviour flyBehaviour;
+
+        void quack()
+        {
+            Console.WriteLine("main duck class quack");
+        }
+        public void performFly()
+        {
+            //    Console.WriteLine("main duck class fly");
+            flyBehaviour.fly();
+        }
+        void display()
+        {
+            Console.WriteLine("this is a main class duck");
+        }   
+    }
+
+    interface FlyBehaviour
+    {
+        void fly();
+    }
+    
+    class NoFly : FlyBehaviour
+    {
+        public void fly()
+        {
+            Console.WriteLine("no fly");
+        }
+    }
+
+    class Rubberduck : Duck
+    {
+        public Rubberduck()
+        {
+            flyBehaviour = new NoFly();   
+        }
+
+
+    }
+}
