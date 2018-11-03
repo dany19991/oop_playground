d35587a59ef7d68539fc71083d5a23db9e05e59c
LAPTOP-DG686SC9\dany <dany19991@yahoo.com>
2018-11-04 00:49:41 +0200

    updated decorator pattern

--- Decorator_pattern/Decorator_pattern/Program.cs
+++ Decorator_pattern/Decorator_pattern/Program.cs
@@ -9,89 +9,99 @@ namespace Decorator_pattern
     class Program
     {
         static void Main(string[] args)
-        {
+        {//0.4f
             Beverage houseBlend = new HouseBlend(); //nu merge daca obiectul creat e de tipul HouseBlend -> 
                                     //nu poate fi folosit in condiment decorator ....care primeste generic Beverage
             Console.WriteLine("Pret : "+ houseBlend.Cost());
+            Console.WriteLine(houseBlend.getDescription());
 
-            houseBlend = new Mocha(houseBlend);
+            houseBlend = new Mocha(houseBlend);//0.3f
+            houseBlend = new Milk(houseBlend);//0.2f
 
+            Console.WriteLine(houseBlend.Cost());
+            Console.WriteLine(houseBlend.getDescription());
         }
     }
-
-    class Beverage
+    
+    abstract class Beverage
     {
-        string description = "None";
-
-        public string getDescription()
-        {
-            return description;
-        }
-
-        public float Cost()
-        {
-            return 0.5f;
-        }
+        public abstract string getDescription();
+        public abstract float Cost();
     }
-
+    
     class HouseBlend : Beverage
     {
-        public new string getDescription()
+        string descriptiontxt = "HouseBlend";
+
+
+        public override float Cost()
         {
-            return "HouseBlend";
+            return 0.4f;
         }
 
-        public new float Cost()
+        public override string getDescription()
         {
-            return 0.2f ;
+            return descriptiontxt;
         }
     }
 
     class Expresso : Beverage
     {
-        public new string getDescription()
+        string descriptiontxt = "Expresso";
+        public override float Cost()
         {
-            return "Expresso";
+            return 0.4f;
         }
 
-        public new float Cost()
+        public override string getDescription()
         {
-            return 0.1f ;
+            return descriptiontxt;
         }
     }
 
     abstract class  CondimentDecorator : Beverage
     {
-        public abstract float Cost();
+        //public abstract override string getDescription();
+        //public abstract override float Cost();
     }
 
     class Mocha : CondimentDecorator
     {
-        Beverage beverage;
+        public Beverage beverage;
 
-        public Mocha( Beverage beverage)
+        public Mocha( Beverage beverage_)
         {
-            this.beverage = beverage;
+            this.beverage = beverage_;
         }
 
         public override float Cost()
         {
             return 0.3f + beverage.Cost();
         }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription()+",mocha";
+        }
     }
 
     class Milk : CondimentDecorator
     {
-        Beverage beverage;
+       public  Beverage beverage;
 
-        public Milk(Beverage beverage)
+        public Milk(Beverage beverage_)
         {
-            this.beverage = beverage;
+            this.beverage = beverage_;
         }
 
         public override float Cost()
         {
             return 0.2f + beverage.Cost();
+        }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription() + ",milk";
         }
     }
     //comment to test older commit message change
