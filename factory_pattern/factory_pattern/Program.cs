69ccc04e5c4cd93d5e63ce528c9b6d9f4f75de67
dany19991 <dany19991@yahoo.com>
2018-11-04 14:38:47 +0200

    added the pattern . wip

--- factory_pattern/factory_pattern/Program.cs
+++ factory_pattern/factory_pattern/Program.cs
@@ -14,4 +14,41 @@ namespace factory_pattern
 
         public void create() { };
     }
+
+
+    class SimplePizzaFactory {
+        
+        public Pizza CreatePizza (string type ){
+            Pizza pizza = null; 
+
+            switch (type)
+            {
+                case "chesee":
+                    pizza = new Pizza(); /*new chesee pizza*/
+                    break;
+            }
+
+            return pizza;
+        }
+    }
+
+    class PizzaStore {
+        SimplePizzaFactory factory;
+
+        public PizzaStore (SimplePizzaFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public Pizza OrderPizza( string type){
+            Pizza pizza;
+
+            pizza = factory.CreatePizza("chesee");
+            pizza.create();
+
+            return pizza;
+        }
+
+
+    }
 }
