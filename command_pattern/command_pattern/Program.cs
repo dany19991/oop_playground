a1f4a0635ed9df7ce1ea73e03cc0d05b3a46b86c
dany19991 <dany19991@yahoo.com>
2019-10-20 15:07:37 +0300

    nush ce am adaugat dar sper sa fie bun

---
+++ command_pattern/command_pattern/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Command command1 = new Command();
+            command1.execute();
+            Command command2 = new ACOn();
+            command2.execute();
+            Command command3 = new Command();
+            try { ((ACOn)command3).execute(); }
+            catch { }
+            Console.WriteLine(((ACOn)command2).getACstate());
+            Console.ReadKey();
+        }
+    }
+    
+    class Command
+    {
+        virtual public void execute() { }
+    }
+
+    class ACOn : Command
+    {
+        private string ACstate = "OFF";
+        public override void execute()
+        {
+            Console.WriteLine("turning on AC");
+            ACstate = "ON";
+        }
+        public void undo()
+        {
+            Console.WriteLine("turning off AC");
+            ACstate = "OFF";
+        }
+
+        public String getACstate()
+        {
+            return ACstate;
+        }
+    }
+
+
+}
+
