eeaf7876b96db80142f91e86e7bcf58233a7a031
dany19991 <dany19991@yahoo.com>
2020-04-06 22:40:04 +0300

    incercare de a face singur/ reface observer pattern

---
+++ observer_pattern_1/observer_pattern_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace observer_pattern_1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+    public class WeatherData
+    {
+        public float getHumidity()
+        { return 0; }
+
+        public float getPressure()
+        { return 0; }
+
+        public float getTemperature()
+        { return 0; }
+
+        public void measurementsChanged()
+        {
+            float temp = getTemperature();
+            float humidity = getHumidity();
+            float pressure = getPressure();
+
+            //display1.update(temp, humidity, pressure);
+            //display2.update(temp, humidity, pressure);
+            //display3.update(temp, humidity, pressure);
+        }
+    }
+
+
+}
