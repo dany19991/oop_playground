dfac5341211acaf79788ebc76ee08bcccd7f20fa
dany19991 <dany19991@yahoo.com>
2020-04-24 01:15:49 +0300

    studiu, incercarea a 2a , repetat factory pattern din headfirts

---
+++ factory_pattern_try2/factory_pattern_try2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace factory_pattern_try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+        }
+        Pizza orderPizza(string type)
+        {
+            Pizza pizza;
+
+            if (type.Equals("cheese"))
+            {
+                pizza = new CheesePizza();
+            }else if (type.Equals("greek"))
+            {
+                pizza = new GreekPizza();
+            } else 
+            {
+                pizza = new Pizza();
+            }
+
+            pizza.prepare();
+            pizza.bake();
+            pizza.cut();
+            pizza.box();
+
+            return pizza;
+        }
+    }
+
+    class Pizza
+    {
+        public void prepare() { }
+        public void bake() { }
+        public void cut() { }
+        public void box() { }
+    }
+
+    class CheesePizza : Pizza
+    { }
+
+    class GreekPizza : Pizza
+    { }
+    
+}
