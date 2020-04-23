68e09e0ffe6f84ad95adfd04b0f3e8194431bc94
dany19991 <dany19991@yahoo.com>
2020-04-24 02:18:15 +0300

    implementare buna factory method

--- factory_pattern_try2/factory_pattern_try2/Program.cs
+++ factory_pattern_try2/factory_pattern_try2/Program.cs
@@ -9,23 +9,19 @@ namespace factory_pattern_try2
         {
             Console.WriteLine("Hello World!");
 
-            SimplePizzaFactory simplePizzaFactory = new SimplePizzaFactory();
-            PizzaStore pizzaStore = new PizzaStore(simplePizzaFactory);
-            Pizza pizza  = pizzaStore.orderPizza("cheese");
+            PizzaStore NYpizzaStore = new NYPizzaStore();
+            Pizza pizza  = NYpizzaStore.orderPizza("cheese");
+            PizzaStore ChpizzaStore = new ChPizzaStore();
+            Pizza pizza1 = ChpizzaStore.orderPizza("greek");
+
         }
     }
 
-
-    public class PizzaStore
+    public abstract class PizzaStore
     {
-        SimplePizzaFactory simplePizzaFactory;
-
-        public PizzaStore (SimplePizzaFactory simplePizzaFactory_)
-        { simplePizzaFactory = simplePizzaFactory_; }
-
         public Pizza orderPizza(string type)
         {
-            Pizza pizza = simplePizzaFactory.createPizza(type);
+            Pizza pizza =createPizza(type);
 
             pizza.prepare();
             pizza.bake();
@@ -34,42 +30,103 @@ namespace factory_pattern_try2
 
             return pizza;
         }
+
+        public abstract Pizza createPizza(string type);
     }
 
-    public class SimplePizzaFactory
+    public class NYPizzaStore : PizzaStore
     {
-        public Pizza createPizza(string type)
+        public override Pizza createPizza(string type)
         {
             Pizza pizza = null;
 
             if (type.Equals("cheese"))
             {
-                pizza = new CheesePizza();
+                pizza = new NYCheesePizza();
             }
             else if (type.Equals("greek"))
             {
-                pizza = new GreekPizza();
+                pizza = new NYGreekPizza();
             }
             else
             {
-                pizza = new Pizza();
+                pizza = new NYPizza();
             }
 
             return pizza;
         }
     }
-    public  class Pizza
+
+    public class ChPizzaStore : PizzaStore
     {
-        public void prepare() { }
+        public override Pizza createPizza(string type)
+        {
+            Pizza pizza = null;
+
+            if (type.Equals("cheese"))
+            {
+                pizza = new ChCheesePizza();
+            }
+            else if (type.Equals("greek"))
+            {
+                pizza = new ChGreekPizza();
+            }
+            else
+            {
+                pizza = new ChPizza();
+            }
+
+            return pizza;
+        }
+    }
+
+
+    public class Pizza
+    {
+        public virtual void prepare() {Console.WriteLine("classic pizza"); } 
+        //daca vreau sa suprascriu o functie in clasele derivate 
+        //trebuie sa fie cel putin virtuala (poate avea implmenetare si in clasa de baza
         public void bake() { }
         public void cut() { }
         public void box() { }
     }
 
     public class CheesePizza : Pizza
-    { }
+    {
+        public override void prepare() { Console.WriteLine("cheese pizza");}
+    }
 
     public class GreekPizza : Pizza
-    { }
-    
+    {
+        public override void prepare() { Console.WriteLine("classic greek pizza"); }
+    }
+    public class NYPizza :Pizza
+    {
+        public override void prepare() { Console.WriteLine("NY pizza"); }
+    }
+
+    public class NYCheesePizza : Pizza
+    {
+        public override void prepare() { Console.WriteLine("NY cheese pizza"); }
+    }
+
+    public class NYGreekPizza : Pizza
+    {
+        public override void prepare() { Console.WriteLine("NY greek pizza"); }
+    }
+        public class ChPizza :Pizza
+    {
+        public override void prepare() { Console.WriteLine("Chicago pizza"); }
+    }
+
+    public class ChCheesePizza : Pizza
+    {
+        public override void prepare() { Console.WriteLine("Chicago cheese pizza"); }
+    }
+
+    public class ChGreekPizza : Pizza
+    {
+        public override void prepare() { Console.WriteLine("Chicago greek pizza"); }
+    }
+
 }
