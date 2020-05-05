2e0818c9f621b3c17d30703821a90d36182f455a
dany19991 <dany19991@yahoo.com>
2020-05-05 23:55:46 +0300

    2 variante de strategy pattern, una simplista, si cea cu rate

---
+++ Strategy_pattern_try2/Strategy_pattern_try2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_pattern_try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //client
+            Context context = new Context();
+            Strategy strategy_type1 = new ConctreteStrategy1();
+            context.setStrategy(strategy_type1);
+            context.doStuff(1);
+            Strategy strategy_type2 = new ConctreteStrategy2();
+            context.setStrategy(strategy_type2);
+            context.doStuff(2);
+        }
+    }
+
+    class Context 
+    {
+        private Strategy _strategy;
+
+        public void setStrategy(Strategy strategy)
+        {
+            _strategy = strategy;
+        }
+        public void doStuff(int data)
+        {
+            _strategy.execute(data);
+        }
+    }
+
+    interface Strategy 
+    {
+        void execute(int data);        
+    }
+
+    class ConctreteStrategy1 : Strategy
+    {
+        public void execute(int data)
+        {
+            Console.WriteLine("this is done by strategy1 and data  is : " + data);
+        }
+    }
+
+    class ConctreteStrategy2 : Strategy
+    {
+        public void execute(int data)
+        {
+            Console.WriteLine("this is done by strategy2 and data  is : " + data);
+        }
+    }
+
+}
---
+++ Strategy_pattern_try3/Strategy_pattern_try3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_pattern_try3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user 
+            Duck rubberDuck = new RubberDuck();
+            rubberDuck.display();
+            rubberDuck.fly();
+            rubberDuck.quack();
+        }
+    }
+
+    interface QuackBehaviour //abstract strategy
+    {
+        void quack();
+    }
+
+    interface FlyBehaviour 
+    {
+        void fly();
+    }
+
+    public class NoFly : FlyBehaviour
+    {
+        public void fly()
+        { Console.WriteLine("Cannot fly");}
+    }
+    public class Fly : FlyBehaviour
+    {
+        public void fly()
+        { Console.WriteLine("Flies away"); }
+    }
+    public class Squeak : QuackBehaviour
+    {
+        public void quack()
+        { Console.WriteLine("Squeak"); }
+    }
+    public class Quack : QuackBehaviour
+    {
+        public void quack()
+        { Console.WriteLine("Quack!");}
+    }
+    public class NoQuack : QuackBehaviour
+    {
+        public void quack()
+        { Console.WriteLine("Silent");}
+    }
+
+    abstract class Duck //context ?
+    {
+        protected QuackBehaviour _quackBehaviour;
+        protected FlyBehaviour _flyBehaviour;
+
+        //public abstract void setFlyBehaviour(FlyBehaviour fly); //facem atribuirea direct in constructor
+        //{ _flyBehaviour = fly; } //facem abstracta sa obligam specificarea unei behaviour , schimbat, vezi mai sus 
+
+        public void setQuackBehaviour(QuackBehaviour quack) //pentru a putea schimba la runtime
+        {_quackBehaviour = quack;}
+
+        public virtual void display() { Console.WriteLine("this is a abstract duck"); }
+        public void quack() { if (_quackBehaviour!=null)  _quackBehaviour.quack(); }
+        public void fly() { if (_flyBehaviour != null) _flyBehaviour.fly(); }
+    }
+
+    class RubberDuck : Duck
+    {   
+        public RubberDuck()
+        {
+            _flyBehaviour = new NoFly();
+            _quackBehaviour = new Squeak();
+        }
+
+        public override void display()
+        { Console.WriteLine("This is a rubber duck"); }
+    }
+
+    class DecoyDuck : Duck
+    {
+        //de pus behaviour si aici 
+        public override void display()
+        { Console.WriteLine("This is a decoy duck"); }
+    }
+
+    class MallardDuck : Duck
+    {
+        //de pus behaviour si aici 
+        public override void display()
+        { Console.WriteLine("This is a Mallard duck"); }
+    }
+}
